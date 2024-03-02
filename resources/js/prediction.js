a688a327c17d671267e8ff571d4f66c0682b426c
allanmanno <allanfrelens@gmail.com>
2024-03-02 11:00:30 -0500

    version 1.4

--- resources/js/prediction.js
+++ resources/js/prediction.js
@@ -1,3 +1,7 @@
 function myFunction() {
-   location.replace ("https://google.com");
+   var a = document.getElementById('states').value;
+   var b = document.getElementById('division').value;
+   var c = "resources/html/" + a + "_" + b + ".html";
+   window.location.assign(c);
+   
 }
